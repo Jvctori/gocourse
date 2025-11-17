617cd37837a6dbd9cf350c76204445617704bb6c
Jvctori <137020639+Jvctori@users.noreply.github.com>
2025-11-16 22:49:12 -0300

    variables and basics commit

---
+++ basics/datatypes.md
@@ -0,0 +1,18 @@
+# __Data Types__
+
+**Integers: int:** 10 var numb int
+**Floating(Pointing Numbers):** 10.0 var numb float64
+**Booleans:** true or false
+**Strings:** "João" var name string
+**Complex Numbers:**
+**Constants:**
+**Arrays:**
+**Structs:**
+**Pointers:**
+**Maps:**
+**Slices:**
+**Functions:**
+**Channels:**
+**Json:**
+**Text and HTML Templates:**
+
--- basics/import.go
+++ basics/import.go
@@ -8,6 +8,10 @@ import (
 // Three shaking and its application
 // three shaking remove modulos de bibliotecas que não estão sendo usados
 // go compiler j́á faz isso para você
+// melhorando a perfomance e garantindo mais eficiencia no controle de dependências
+// - pacotes, - runtime
+// exp: React e Angular (Ambos possuem three shaking, eliminando dead code)
+// ao usar "go run ." || "go build ." executamos o three shaking e removemos dead code
 func main() {
 	fmt.Println("Hello, Go Standard Library")
 
