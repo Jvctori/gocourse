a97bb765c34e37a5daa30d0a887990055bc940f1
Jvctori <137020639+Jvctori@users.noreply.github.com>
2025-11-19 15:22:27 -0300

    ...

---
+++ constants.go
@@ -0,0 +1,19 @@
+package main
+
+const PI = 3.14
+const GRAVITY = 9.81
+// CONSTANTES ACESSIVEIS GLOBALMENTE!
+// CONSTANTES SÃO VARIAVEIS DECLARADAS COM A INTENÇÃO DE SEREM IMUTAVEIS
+func main() {
+	// CONSTANTE TIPADA
+	const DAYS int = 7
+	// CONSTANTES DECLARADA COMO UM "CONJUNTO"
+	// Constantes não podem ser declaradas dinamicamente
+	// exemplo: constant_name := 12
+	const (
+		monday = 1
+		tuesday = 2
+		wesnday = 3 
+		thursday = 4
+	)
+}
