3ef9f7f313bf6cb89731a36c42ded2882b68cebb
Jvctori <137020639+Jvctori@users.noreply.github.com>
2025-11-19 17:45:21 -0300

    ...

--- arimetic_operators.go
+++ arimetic_operators.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	// Variables declaration
@@ -42,6 +45,7 @@ func main() {
 	fmt.Println(maxInt)*/
 	// output: ./arimetic_operators.go:41:21: cannot use 92233221314127231823912746123617 (untyped int constant) as int64 value in variable declaration (overflows)
 	// o valor maximo para int64 é: 9223372036854775807
+	// OVERFLOW COM INT64
 	var maxInt int64 = 9223372036854775807 // int64 max value
 	fmt.Println("Valor maximo do int64:", maxInt)
 	maxInt = (maxInt + 1)
@@ -51,4 +55,12 @@ func main() {
 	fmt.Println("Valor maximo de unsigned int (uint64):", uMaxInt)
 	uMaxInt = (uMaxInt + 1)
 	fmt.Println("O que acontece se adicionar +1 ao uint64:", uMaxInt)
+
+	var smallFloat float64 = 1.0e-323
+	fmt.Println(smallFloat)
+	smallFloat = (smallFloat / math.MaxFloat64)
+	fmt.Println(smallFloat)
+	// UNDERFLOW with floating numbers:
+	// output foi 0 pois é uma divisão muito grande, ou seja:...
+	// não há possiblidade do float64 representar esse valor muito pequeno
 }
